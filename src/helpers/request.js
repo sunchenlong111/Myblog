12d318687d6fbde3479ce18375282021e91ec068
sunchenlong111 <1094584419@qq.com>
2018-10-27 04:03:33 +0800

    blog-cuth-helper

---
+++ src/helpers/request.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.baseURL = " http://blog-server.hunger-valley.com"
+axios.defaults.withCredentials = true 
+//通过设置withCredentials 为true获得的第三方cookies，将会依旧享受同源策略
+
+export default function request(url,type = 'GET',data={}){
+  return new Promise((resolve,reject)=>{
+    let option = {
+      url,
+      method: type
+    }
+    if(type.toLowerCase() === 'get'){
+      option.params = data
+    }else{
+      option.data = data
+    }
+    axios(option).then(res=>{
+      if(res.data.status === "ok") {
+        console.log(res.data)
+        resolve(res.data)
+      }else{
+        Message.error(res.data.msg)
+        reject(res.data)
+      }
+    }).catch(err=>{
+      Message.err('网络异常')
+      reject({msg:'网络异常'})
+    })
+  })
+}
--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,6 +1,14 @@
+import request from '@/helpers/request.js'
+import auth from '@/api/auth'
+import blog from '@/api/blog'
+
+window.request = request
+window.auth = auth
+window.blog = blog
+
 export default {
   data() {
-
+    return {}
   },
 
   methods: {
